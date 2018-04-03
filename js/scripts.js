86157b8fad6d3d4b0a04384a0d639bc4e9371769
Danuta <danprzy@gmail.com>
2018-04-03 16:37:19 +0200

    Add task 8-5

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var a = 5;
+var h = 4;
+var triangleArea =a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+/*script with prompt
+var a = prompt("Podaj długość jednego boku trójkąta (a)");
+var h = prompt("Podaj długość wysokości (h) opuszczonej na bok a");
+var triangleArea =a*h/2;
+alert('Powierzchnia trójkąta wynosi, ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+*/
