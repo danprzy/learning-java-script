56088af8c2fd7e41c9ff079ae7838c5c6a3e44b4
Danuta <danprzy@gmail.com>
2018-04-05 23:56:25 +0200

    Add ternary operator

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,10 @@ var a = 5;
 var b = 1;
 var value = (a * a) - (2 * a * b) - (b * b);
 console.log(value);
+//ternary operator
+value > 0 ? document.write('Wynik dodatni') : document.write('Wynik ujemny');
 
+/*
 if (value > 0) {
     document.write('Wynik dodatni');
 } else if (value == 0) {
@@ -10,4 +13,4 @@ if (value > 0) {
 } else {
     document.write('Wynik ujemny');
 }
-
+*/
