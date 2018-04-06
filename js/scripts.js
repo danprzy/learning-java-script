f302397d753ed6fe0fadea77b755af928f449613
Danuta <danprzy@gmail.com>
2018-04-06 11:19:27 +0200

    Change ternary operator

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,19 @@ var a = 5;
 var b = 1;
 var value = (a * a) - (2 * a * b) - (b * b);
 console.log(value);
-//ternary operator
-value > 0 ? document.write('Wynik dodatni') : document.write('Wynik ujemny');
 
-/*
+// Method A
+if (value > 0) {
+    document.write('Wynik dodatni');
+} else {
+value == 0 ? document.write('Wynik jest równy 0') : document.write('Wynik ujemny');
+};
+
+/* Method B
+value > 0 ? document.write('Wynik dodatni') : (value == 0 ? document.write('Wynik jest równy 0') : document.write('Wynik ujemny'));
+*/
+
+/* Method C
 if (value > 0) {
     document.write('Wynik dodatni');
 } else if (value == 0) {
